d2175ce967a708c02962b67ca87d85e098da540f
LuisVillela <103969295+LuisVillela@users.noreply.github.com>
2022-05-15 21:52:41 -0600

    minimax Algorithm

--- tic_tac_toe/utils.py
+++ tic_tac_toe/utils.py
@@ -67,11 +67,57 @@ def read_board() -> list:
 
 def decide_move(board: list, player_id: str) -> [int, int]:
     """
-    Decides next move to make.
+    Decides next move to make. ###AQUI VA MI CODIGO
     """
-    row = randint(0, 2)
-    column = randint(0, 2)
-    return [row, column]
+    # row = randint(0, 2)
+    # column = randint(0, 2)
+    # return [row, column]
+
+#mi codigo
+    def_init_(self, letter):
+        super()._init_(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else:
+            square = self.minimax(game, self.letter)["position"]
+        return [square]
+
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player = "0" if player == "X" else "X"
+
+        #first we want to check if the previous move is the winner
+        if state.current_winner == other_player
+            return {"position": None, "score": 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (state.num_empty_squares()+1)}
+        elif not state.empty_squares():
+            return {"position": None, "score": 0}
+
+        if player == max_player:
+            best = {"position": None, "score": -math.inf}
+        else:
+            best = {"position": None, "score": math.inf}
+        for possible_move in state.available_moves():
+            state.make_move(possible_move, player)
+            sim_score = self.minimax(state, other_player) #simulate a game after making that move
+
+            #undo move
+            state.board[possible_move] = " "
+            state.current_winner = None
+            sim_score["position"] = possible_move # this represents the move optimal next move
+
+            if player == max_player:
+                if sim_score["score"] > best["score"]:
+                    best = sim_score
+
+            else:
+                if sim_score["score"] < best["score"]:
+                    best = sim_score
+        
+        return best
+
+
 
 
 def validate_move(board: list, move: list) -> bool:
